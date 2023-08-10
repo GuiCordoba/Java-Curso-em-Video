56f1938f215e8b8edbb8b5aa21aba435d1c8fd98
Guilherme <guimcordoba@hotmail.com>
2023-08-10 17:20:32 -0300

    Aula03 adicionado

---
+++ Exercicios/Aula03/src/CursoEmVideo/OperadoresAritimeticos.java
@@ -0,0 +1,57 @@
+package CursoEmVideo;
+
+public class OperadoresAritimeticos {
+
+	public static void main(String[] args) {
+		/*int n1 = 3;
+		int n2 = 9;
+		float m = (n1 + n2) / 2;
+		System.out.println("A media é igual a " + m);
+		
+		//Operadores Unitarios
+		m ++;
+		System.out.println(m);
+		m --;
+		System.out.println(m);
+		
+		int n = 5;
+		int valor = 5 + ++n;
+		System.out.println(valor);
+		System.out.println(n);
+		
+		//Operadores de Atribuição
+		int x = 4;
+		x += 2;
+		System.out.println(x);
+		x = 5;
+		x *= 2;
+		System.out.println(x);
+		
+		//Classe Math
+		double raizQuadrada = Math.sqrt(25);
+		System.out.println(raizQuadrada);
+		
+		double potencia = Math.pow(2,10);
+		System.out.println(potencia);
+		
+		//Arredondamentos
+		float baixo = 8.3f;
+		int ar = (int) Math.floor(baixo);
+		System.out.println(ar);
+		float cima = 4.6f; 
+		int ar1 = (int)Math.ceil(cima);
+		System.out.println(ar1);*/
+		
+		//Gerador de numeros
+		double aleatorio = Math.random();
+		int n = (int) (5 + aleatorio * (10-5)); //aleatorio entre um intervalo especifico
+		System.out.println(n);
+		
+				
+		
+		
+		
+
+	}
+
+}
