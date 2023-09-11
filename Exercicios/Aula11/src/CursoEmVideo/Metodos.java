b14a00f440be256e30e5ab38eb22cf8245556fc3
Guilherme <guimcordoba@hotmail.com>
2023-09-11 17:17:38 -0300

    Inserção da aula11

---
+++ Exercicios/Aula11/src/CursoEmVideo/Metodos.java
@@ -0,0 +1,13 @@
+package CursoEmVideo;
+
+public class Metodos {
+	
+
+	public static void main(String[] args) {
+		System.out.println("Vai comecar a contagem contagem");
+		
+		System.out.println(Operacoes.contador(1, 5));
+
+	}
+
+}
