88e53130099931b09475ba44f03694eea4d9482a
Guilherme <guimcordoba@hotmail.com>
2023-09-12 13:17:27 -0300

    ExercicioFinal adicionado

--- Exercicios/Aula11/src/CursoEmVideo/Metodos.java
+++ Exercicios/Aula11/src/CursoEmVideo/Metodos.java
@@ -5,7 +5,6 @@ public class Metodos {
 
 	public static void main(String[] args) {
 		System.out.println("Vai comecar a contagem contagem");
-		
 		System.out.println(Operacoes.contador(1, 5));
 
 	}
---
+++ Exercicios/ExercicioFinal/src/CursoEmVideo/Funcao.java
@@ -0,0 +1,14 @@
+package CursoEmVideo;
+
+public class Funcao {
+
+	public static void main(String[] args) {
+		Fatorial f = new Fatorial();
+		f.setValor(5);
+		System.out.print(f.getFormula());
+		System.out.println(f.getFatorial());
+		
+
+	}
+
+}
